3b6ea994664914ea39a1e9710dd0e165d6887180
Oleg Mikhnenkov <Oleg.Mikhnenkov@cyberloop.ai>
2024-11-12 21:10:19 +0700

    fix: typo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'premium-coctails'`, () => {
+  it(`should have as title 'premium-cocktails'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('premium-coctails');
+    expect(app.title).toEqual('premium-cocktails');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('premium-coctails app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('premium-cocktails app is running!');
   });
 });
